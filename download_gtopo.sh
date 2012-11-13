3be4f50c42cd5cc74070a202d7136d272120da0e
Nick Lebedev <nlebedev@gispro.ru>
2012-11-13 15:49:12 +0400

    download_gtopo.sh
    Скачиваем GTOPO

---
+++ download_gtopo.sh
@@ -0,0 +1,16 @@
+#Скачиваем РФ до 180-го меридиана
+wget http://edcftp.cr.usgs.gov/pub/data/gtopo30/global/e020n90.tar.gz
+wget http://edcftp.cr.usgs.gov/pub/data/gtopo30/global/e060n90.tar.gz
+wget http://edcftp.cr.usgs.gov/pub/data/gtopo30/global/e100n90.tar.gz
+wget http://edcftp.cr.usgs.gov/pub/data/gtopo30/global/e140n90.tar.gz
+
+#Отдельно скачиваем Чукотку :)
+wget http://edcftp.cr.usgs.gov/pub/data/gtopo30/global/w180n90.tar.gz
+
+#Распаковываем архивы
+tar -xvf e020n90.tar.gz
+tar -xvf e060n90.tar.gz
+tar -xvf e100n90.tar.gz
+tar -xvf e140n90.tar.gz
+tar -xvf w180n90.tar.gz
+
